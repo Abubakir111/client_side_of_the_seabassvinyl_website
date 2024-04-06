c8b91c39d81e0d66588fb7286b550bbfeea416a5
Бакир Базаркул <bakirbazarkul16@gmail.com>
2024-04-06 10:36:19 +0300

    доделал анимацию блока меню

--- script/index.js
+++ script/index.js
@@ -4,7 +4,7 @@ const animateBtnHeader = document.querySelector('#block-button__aninamtion');
 const animateBtnHeaderText = document.querySelector('#block-button__text-span');
 const headerBurgerClickOpen = document.querySelector('#header__button-burger');
 const headerMenuBurgeblock = document.querySelector('#header__menu');
-const headerMenuBlockClickClose = document.querySelector('#header___menu-icon');
+const headerMenuBlockClickClose = document.querySelector('#header__menu-icon');
 
 button.addEventListener('mouseover', (e) => {
   Btnanimation(e, animateBtnHeader, animateBtnHeaderText, '#cf2e2e');
@@ -16,9 +16,28 @@ button.addEventListener('mouseout', (e) => {
 
 headerBurgerClickOpen.addEventListener('click', () => {
   headerMenuBurgeblock.style = `width: 100%;`;
-  headerMenuBurgeblock.querySelector('.header___Burger-container').style.display = 'block';
+  headerMenuBurgeblock.querySelector('.header__menu-container').style.display = 'block';
+  setTimeout(() => {
+    headerMenuBurgeblock.querySelectorAll('.header__menu-nav-itam').forEach((e) => {
+      e.style = ' transition-duration: 1s; transform: translateY(0%); ';
+    });
+    setTimeout(() => {
+      document.querySelector('.header__menu-column2').style = 'opacity:1';
+    }, 500);
+  }, 500);
 });
 headerMenuBlockClickClose.addEventListener('click', () => {
-  headerMenuBurgeblock.style = `width: 0%;`;
-  headerMenuBurgeblock.querySelector('.header___Burger-container').style.display = 'none';
+  setTimeout(() => {
+    headerMenuBurgeblock.style = `width: 0%;`;
+    headerMenuBurgeblock.querySelector('.header__menu-container').style.display = 'none';
+    headerMenuBurgeblock.querySelectorAll('.header__menu-nav-itam').forEach((e) => {
+      e.style = ' transition-duration: 1s; transform: translateY(100%);';
+    });
+  }, 1000);
+  setTimeout(() => {
+    headerMenuBurgeblock.querySelectorAll('.header__menu-nav-itam').forEach((e) => {
+      e.style = ' transition-duration: 1s; transform: translateY(-100%); ';
+    });
+  }, 500);
+  document.querySelector('.header__menu-column2').style = 'opacity:0';
 });
